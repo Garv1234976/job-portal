ee6c669a5defeca3b7bbd5226ef0c61e4e0f658c
pooja639808 <poojakashyap639808@gmail.com>
2026-04-09 14:44:34 +0530

    api changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://server.budes.online/api"
+    baseURL: "https://server.budes.online/public_html/api"
 });
 
 // 🔥 ADD THIS INTERCEPTOR
