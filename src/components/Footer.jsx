6adf256ab70d9088bd15dd7cfb1259f7e5f7432c
pooja639808 <poojakashyap639808@gmail.com>
2026-04-12 10:33:28 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,8 @@
 function Footer() {
   return (
     <div className="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
-
       <div className="container py-5">
         <div className="row g-5">
-
-          {/* Company Info */}
           <div className="col-lg-4 col-md-6">
             <h5 className="text-white mb-4">Business Buddies</h5>
 
@@ -31,13 +28,30 @@ function Footer() {
 
             {/* Social */}
             <div className="d-flex pt-2">
-              <a className="btn btn-outline-light btn-social" href="#">
+              <a
+                className="btn btn-outline-light btn-social"
+                href="https://www.facebook.com/businessbuddies"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fab fa-facebook-f"></i>
               </a>
-              <a className="btn btn-outline-light btn-social" href="#">
+
+              <a
+                className="btn btn-outline-light btn-social"
+                href="https://www.linkedin.com/company/business-buddies"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fab fa-linkedin-in"></i>
               </a>
-              <a className="btn btn-outline-light btn-social" href="#">
+
+              <a
+                className="btn btn-outline-light btn-social"
+                href="https://www.instagram.com/businessbuddies"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fab fa-instagram"></i>
               </a>
             </div>
@@ -47,20 +61,28 @@ function Footer() {
           <div className="col-lg-4 col-md-6">
             <h5 className="text-white mb-4">Useful Links</h5>
 
-            <a className="btn btn-link text-white-50" href="/about">About Us</a>
-            <a className="btn btn-link text-white-50" href="/contact">Contact Us</a>
-            <a className="btn btn-link text-white-50" href="#">Services</a>
-            <a className="btn btn-link text-white-50" href="#">Privacy Policy</a>
-            <a className="btn btn-link text-white-50" href="#">Terms & Condition</a>
+            <a className="btn btn-link text-white-50" href="/about">
+              About Us
+            </a>
+            <a className="btn btn-link text-white-50" href="/contact">
+              Contact Us
+            </a>
+            <a className="btn btn-link text-white-50" href="#">
+              Services
+            </a>
+            <a className="btn btn-link text-white-50" href="#">
+              Privacy Policy
+            </a>
+            <a className="btn btn-link text-white-50" href="#">
+              Terms & Condition
+            </a>
           </div>
 
           {/* Newsletter */}
           <div className="col-lg-4 col-md-6">
             <h5 className="text-white mb-4">Newsletter</h5>
 
-            <p>
-              Subscribe to get updates about jobs, services, and offers.
-            </p>
+            <p>Subscribe to get updates about jobs, services, and offers.</p>
 
             <div
               className="position-relative mx-auto"
@@ -79,44 +101,26 @@ function Footer() {
                 Sign Up
               </button>
             </div>
-
-            {/* WhatsApp CTA */}
-            <div className="mt-3">
-              <a
-                href="https://wa.me/919478391355"
-                target="_blank"
-                rel="noreferrer"
-                className="btn btn-success w-100"
-              >
-                Chat on WhatsApp
-              </a>
-            </div>
           </div>
-
         </div>
       </div>
 
       {/* Bottom Section */}
       <div className="container border-top border-secondary pt-3">
         <div className="row">
-
           <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
             © {new Date().getFullYear()}{" "}
-            <span className="text-white">Business Buddies</span>, All Rights Reserved.
+            <span className="text-white">Business Buddies</span>, All Rights
+            Reserved.
           </div>
 
           <div className="col-md-6 text-center text-md-end">
-            
             <div className="footer-menu">
               <a href="/">Home</a>
-              <a href="#">Help</a>
-              <a href="#">FAQs</a>
             </div>
           </div>
-
         </div>
       </div>
-
     </div>
   );
 }
