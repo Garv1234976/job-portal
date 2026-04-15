ac83e1bbd801a8fe26c2dfb04dadc9849582f3d6
pooja639808 <poojakashyap639808@gmail.com>
2026-04-15 13:21:51 +0530

    changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,45 +1,46 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function Category() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    API.get("/categories").then((res) => {
+      setCategories(res.data.data || []);
+    });
+  }, []);
+
   return (
     <div className="container-xxl py-5">
       <div className="container">
 
-        <h1 className="text-center mb-5">
-          Explore By Category
-        </h1>
+        <h1 className="text-center mb-5">Explore By Category</h1>
 
         <div className="row g-4">
 
-          <div className="col-lg-3 col-sm-6">
-            <a className="cat-item rounded p-4" href="#">
-              <i className="fa fa-3x fa-mail-bulk text-primary mb-4"></i>
-              <h6 className="mb-3">Marketing</h6>
-              <p className="mb-0">123 Vacancy</p>
-            </a>
-          </div>
+          {categories.map((cat) => (
+            <div className="col-lg-3 col-sm-6" key={cat.id}>
+              <div className="cat-item rounded p-4">
 
-          <div className="col-lg-3 col-sm-6">
-            <a className="cat-item rounded p-4" href="#">
-              <i className="fa fa-3x fa-headset text-primary mb-4"></i>
-              <h6 className="mb-3">Customer Service</h6>
-              <p className="mb-0">123 Vacancy</p>
-            </a>
-          </div>
+                <i className={`fa fa-3x ${cat.icon} text-primary mb-4`}></i>
 
-          <div className="col-lg-3 col-sm-6">
-            <a className="cat-item rounded p-4" href="#">
-              <i className="fa fa-3x fa-user-tie text-primary mb-4"></i>
-              <h6 className="mb-3">Human Resource</h6>
-              <p className="mb-0">123 Vacancy</p>
-            </a>
-          </div>
+                <h6 className="mb-3">{cat.name}</h6>
 
-          <div className="col-lg-3 col-sm-6">
-            <a className="cat-item rounded p-4" href="#">
-              <i className="fa fa-3x fa-tasks text-primary mb-4"></i>
-              <h6 className="mb-3">Project Management</h6>
-              <p className="mb-0">123 Vacancy</p>
-            </a>
-          </div>
+                <ul className="list-unstyled small">
+                  {cat.children.slice(0, 3).map((sub) => (
+                    <li key={sub.id}>• {sub.name}</li>
+                  ))}
+                </ul>
+
+                {cat.children.length > 3 && (
+                  <small className="text-primary">
+                    +{cat.children.length - 3} more
+                  </small>
+                )}
+
+              </div>
+            </div>
+          ))}
 
         </div>
 
