90a71a763724b2a26227180f363c3d2ff2b9e9c8
pooja639808 <poojakashyap639808@gmail.com>
2026-04-25 11:29:35 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://server.budes.online/api",
+    baseURL: "https://server.budes.online/api"
 });
 
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            const currentPath = window.location.pathname;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-            if (currentPath !== "/login") {
-                localStorage.removeItem("token");
-                window.location.href = "/login";
-            }
-        }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-        return Promise.reject(error);
-    },
-);
+    return config;
+});
 
 export default api;
