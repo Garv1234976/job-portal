49e70e99a57b3c3a16645fc1bba0999773ef3a02
pooja639808 <poojakashyap639808@gmail.com>
2026-04-29 15:10:44 +0530

    changes

--- src/pages/Recruiter/CreateJob.jsx
+++ src/pages/Recruiter/CreateJob.jsx
@@ -527,7 +527,6 @@ export default function CreateJob() {
                           <option value="">Select Unit</option>
                           <option value="year">Year</option>
                           <option value="month">Month</option>
-                          <option value="both">Year / Month</option>
                         </select>
                       </div>
                     </div>
